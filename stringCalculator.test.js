1ee2fddfabe4f6ca71c4a6aaed7b2d33b8e3aa13
PurviVaghela <purvi09v@gmail.com>
2024-10-21 12:22:05 +0530

    test for single num

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -2,7 +2,16 @@ import { expect } from 'chai';
 import { add } from './stringCalculator.js';
 
 describe('String Calculator', () => {
+
+    // empty string
     it('should return 0 for an empty string', () => {
         expect(add("")).to.equal(0);
     });
+
+    // single number
+    it('should return the number itself for a single number', () => {
+         expect(add("1")).to.equal(1);
+     });
+
+
 });
