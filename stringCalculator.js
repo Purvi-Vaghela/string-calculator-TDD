3dee296e596fef79261e89ea6c113088ae043843
PurviVaghela <purvi09v@gmail.com>
2024-10-21 13:42:31 +0530

    code for custom-delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -9,14 +9,23 @@ export function add(numbers) {
         return parseInt(numbers, 10); // Convert the string to a number
     }
       // sum of two numbers 
-      const numArray = numbers.split(",");
+    //   const numArray = numbers.split(",");
     //   return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 
     // sum of multiple numbers having new line as delimiter
      const numHavingNewLineDelimiter = numbers.split(/[\n,]/);
-     return numHavingNewLineDelimiter.reduce((sum, num) => sum + parseInt(num), 0);
-
-     //
+    //  return numHavingNewLineDelimiter.reduce((sum, num) => sum + parseInt(num), 0);
+    
+    let delimiter = /[\n,]/;
+     //custom delimiter
+       //"//;\n1;2" :-->>  first part: //;  second: 1;2 
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(`[${parts[0].slice(2)}]`);
+    numbers = parts[1];
+}
+    const numArray = numbers.split(delimiter);
+     return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-// console.log(add("1\n2,3"));
+console.log(add("//;\n1;2"));
